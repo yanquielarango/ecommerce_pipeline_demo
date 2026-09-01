64f2fd4f0eb8048ac1c7dd877db4bd37ba275889
yanquielarango <yanquiel.arango@gmail.com>
2026-09-02 00:04:34 +0200

    Add customer state location for Power BI map

--- ecommerce_pipeline_demo/src/transformations/gold/dim_customer_functions.py
+++ ecommerce_pipeline_demo/src/transformations/gold/dim_customer_functions.py
@@ -5,6 +5,7 @@ def build_dim_customer(df):
     return (
         df.filter(F.col("__END_AT").isNull())
         .withColumn("customer_key", F.xxhash64("customer_id"))
+        .withColumn("customer_state_location", F.concat_ws(", ", F.col("customer_state"), F.lit("Brazil")))
         .select(
             "customer_key",
             "customer_id",
@@ -12,5 +13,6 @@ def build_dim_customer(df):
             "customer_zip_code_prefix",
             "customer_city",
             "customer_state",
+            "customer_state_location",
         )
     )
